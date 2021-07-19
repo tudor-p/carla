9dc472a82bf3f66dadf3cd1c65a69aa1b00b8629
tudor-p <jq.tudor@gmail.com>
2021-07-19 20:25:19 +0100

    project complete

--- diagram2.js
+++ diagram2.js
@@ -6,43 +6,38 @@ const seed2 = (sketch) => {
 
     let povRadius = 25
 
-    let currentImageIndex = 1
-
     sketch.setup = () => {
         
-        for (let i = 1; i < 20; i++) frontImages[i] = sketch.loadImage("images/front" + i + ".png")
-        for (let i = 1; i < 16; i++) backImages[i] = sketch.loadImage("images/back" + i + ".png")
-        for (let i = 1; i < 19; i++) leftImages[i] = sketch.loadImage("images/left" + i + ".png")
-        for (let i = 1; i < 18; i++) rightImages[i] = sketch.loadImage("images/right" + i + ".png")
-        
+        for (let i = 1; i <= 15; i++) {
+            frontImages[i] = sketch.loadImage("images/front" + i + ".png")
+            backImages[i] = sketch.loadImage("images/back" + i + ".png")
+            leftImages[i] = sketch.loadImage("images/left" + i + ".png")
+            rightImages[i] = sketch.loadImage("images/right" + i + ".png")
+        }
+
         sketch.frameRate(5)
 
-        sketch.createCanvas(1920, 1080)
-        POVs[0] = new POV(0.5*sketch.width, 0.05*sketch.height, povRadius, 'north')
-        POVs[1] = new POV(0.95*sketch.width, 0.6*sketch.height, povRadius, 'east')
-        POVs[2] = new POV(0.4*sketch.width, 0.95*sketch.height, povRadius, 'south')
-        POVs[3] = new POV(0.05*sketch.width, 0.55*sketch.height, povRadius, 'west')
+        sketch.createCanvas(sketch.displayWidth, 1080)
+        POVs[0] = new POV(0.5*sketch.width, 0.05*sketch.height, povRadius, 'north', backImages)
+        POVs[1] = new POV(0.95*sketch.width, 0.6*sketch.height, povRadius, 'east', rightImages)
+        POVs[2] = new POV(0.4*sketch.width, 0.95*sketch.height, povRadius, 'south', frontImages)
+        POVs[3] = new POV(0.05*sketch.width, 0.55*sketch.height, povRadius, 'west', leftImages)
 
         // sketch.noLoop()
     }
     
     sketch.draw = () => {
         sketch.background(255)
-
-        sketch.image(frontImages[currentImageIndex], 0, 0)
-        sketch.image(backImages[currentImageIndex], 50, 50)
         
         POVs.forEach(POV => {
+            POV.onClick()
             POV.display()
         })
-        
-        if (currentImageIndex < 19) {
-            currentImageIndex++ 
-        } 
+ 
     }
 
     class POV {
-        constructor (posX, posY, radius, name) {
+        constructor (posX, posY, radius, name, imageArray) {
             this.name = name
             this.x = posX
             this.y = posY
@@ -50,6 +45,8 @@ const seed2 = (sketch) => {
             this.opacity = sketch.random(50, 85)
             this.pointA = []
             this.pointB = []
+            this.currentImageIndex = 1
+            this.imageArray = imageArray
         }
 
         display = () => {
@@ -59,8 +56,6 @@ const seed2 = (sketch) => {
             
             // building the triangles
             
-            sketch.noStroke()
-            sketch.fill(225, 225, 225, this.opacity)
             switch (this.name) {
                 case 'north':
                     this.pointA.push(this.x - sketch.width * 0.25, this.y + sketch.height * 0.75)
@@ -83,12 +78,27 @@ const seed2 = (sketch) => {
                     break
             }
 
+            sketch.noStroke()
+            sketch.fill(225, 225, 225, this.opacity)
             sketch.triangle(this.x, this.y, this.pointA[0], this.pointA[1], this.pointB[0], this.pointB[1])
           
         }
 
         onClick = () => {
-            console.log(this.x, this.y)
+            let distance = sketch.dist(sketch.mouseX, sketch.mouseY, this.x, this.y)
+            
+            sketch.image(this.imageArray[this.currentImageIndex], 0, 0)
+
+            if (distance < povRadius / 2) {
+                
+                if (this.currentImageIndex < 15) {
+                    this.currentImageIndex++
+                } else {
+                    this.currentImageIndex = 1
+                }
+
+            }
+
         }
     }
 }
